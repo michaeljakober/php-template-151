1e68ae49482374d6c864d5f60056bda2b07b3a2b
MichaelJakober <michael.jakober2000@gmail.com>
2017-06-18 16:11:26 +0200

    Added schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+-- Adminer 4.2.5 MySQL dump
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -30,4 +32,21 @@ CREATE TABLE `word` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `word` (`Id`, `word`) VALUES
-(1,	'Hangman');
+(1,	'Hangman'),
+(2,	'Ei'),
+(3,	'zu'),
+(4,	'Der'),
+(5,	'Das'),
+(6,	'Haus'),
+(7,	'Baum'),
+(8,	'Sechs'),
+(9,	'Handy'),
+(10,	'Alpaka'),
+(11,	'Orange'),
+(12,	'Tabelle'),
+(13,	'Haarband'),
+(14,	'Schranke'),
+(15,	'Tischbein'),
+(16,	'Stuhlbein');
+
+-- 2017-06-18 14:09:06
